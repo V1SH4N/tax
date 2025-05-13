187cfffc79e6f046ee4fe00e6bc436bc1111e4ea
V1SH4N <vishandaby305@gmail.com>
2025-05-13 19:16:24 +0400

    bonjour

---
+++ script.js
@@ -0,0 +1,118 @@
+const canvas = document.getElementById("receiptCanvas");
+const ctx = canvas.getContext("2d");
+const morning = document.getElementById("morning");
+const afternoon = document.getElementById("afternoon");
+const downloadBtn = document.getElementById("downloadBtn");
+const back = document.getElementById("back")
+
+function getCurrentDay() {
+    const now = new Date();
+    return now.toLocaleDateString("en-GB", {
+      weekday: "long"
+    });
+  }
+  
+function getFormattedDate() {
+  const date = new Date();
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = date.toLocaleString("en-GB", { month: "long" });
+  const year = date.getFullYear();
+  return `${day} ${month} ${year}`;
+}
+
+function drawHorizontallyCenteredText(ctx, text, centerX, y) {
+    // Set the font before measuring
+    ctx.font = "600 69px 'IBM Plex Sans'";
+  
+    // Measure the width of the text
+    const textWidth = ctx.measureText(text).width;
+  
+    // Calculate the X position so the text is centered
+    const x = centerX - (textWidth / 2);
+  
+    // Draw the text
+    ctx.fillText(text, x, y);
+  }
+  
+
+function drawTable(day, date, isMorning) {
+  canvas.width = 4419;
+  canvas.height = 6250;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  const loadImage = src => new Promise(resolve => {
+    const img = new Image();
+    img.src = src;
+    img.onload = () => resolve(img);
+  });
+
+  const loadFonts = () => {
+    return document.fonts.load("600 69px 'IBM Plex Sans'").then(() => document.fonts.ready);
+  };
+  
+  
+
+  Promise.all([
+    loadImage("./images/morning.png"),
+    loadImage("./images/afternoon.png"),
+    loadFonts()
+  ]).then(([morning, afternoon]) => {
+
+    ctx.fillStyle = "#FFFFFF";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    if (isMorning){
+        ctx.drawImage(morning, 0, 0);
+    }else{
+        ctx.drawImage(afternoon, 0, 0);
+    }
+
+    ctx.font = "600 69px 'IBM Plex Sans', sans-serif";
+    ctx.fillStyle = "black";
+    drawHorizontallyCenteredText(ctx, "Wednedsay", 2565, 5555);
+    ctx.fillText(date, 2359, 5650);
+    
+    morning.style.display = "none";
+    afternoon.style.display = "none";
+    downloadBtn.style.display = "block";
+    back.style.display = "block";
+  });
+}
+
+
+
+morning.addEventListener("click", function (e) {
+  const date = getFormattedDate();
+  const day = getCurrentDay();
+  const isMorning = true;
+  drawTable(day, date, isMorning);
+});
+
+afternoon.addEventListener("click", function (e) {
+    const date = getFormattedDate();
+    const day = getCurrentDay();
+    const isMorning = false;
+    drawTable(day, date, isMorning);
+  });
+
+downloadBtn.addEventListener("click", function () {
+  canvas.toBlob(function (blob) {
+    const a = document.createElement("a");
+    a.download = "canvas-image.png";
+    a.href = URL.createObjectURL(blob);
+    a.click();
+  }, "image/png");
+
+  downloadBtn.style.display = "none";
+  back.style.display = "none";
+  morning.style.display = 'block';
+  afternoon.style.display = 'block';
+});
+
+
+back.addEventListener('click', (e) => {
+    downloadBtn.style.display = "none";
+    back.style.display = "none";
+    morning.style.display = 'block';
+    afternoon.style.display = 'block';
+})
